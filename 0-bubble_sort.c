6b4f2dfbcefe773babf9b7938ad5fbf2fb520054
Alphonce Kochieng <dulkochieng@gmail.com>
2023-09-27 09:06:31 +0000

    update the bubble sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -22,12 +22,12 @@ void bubble_sort(int *array, size_t size)
 				array[cur] = array[prev] ^ array[cur];
 				array[prev] = array[prev] ^ array[cur];
 				change++;
+				print_array(array, size);
 			}
 			if (cur == size - 1 && change == 1)
 				change = 0;
 			else if (cur == size - 1 && change > 1)
 				change = 1;
 		}
-		print_array(array, size);
 	}
 }
